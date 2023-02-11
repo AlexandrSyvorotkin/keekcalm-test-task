a7049324ac7d09419c8fe8c841020e43f1c1a3d2
Your Name <you@example.com>
2023-02-11 19:00:20 +0300

    minor changes

--- src/Layout/Layout/Layout.jsx
+++ src/Layout/Layout/Layout.jsx
@@ -13,7 +13,7 @@ const Layout = () => {
 		<div>
 			<Description
                 img={img1}
-                short_text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                short_text='Проект киберспортивного турнира "7 холмов" реализованного в рамках '
                 full_text='Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
             />
 			<DescriptionReverse
